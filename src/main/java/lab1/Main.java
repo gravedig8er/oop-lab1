1ea08703675ac7f023baeb3fcd839e019cc728b8
gravedig8er <froppee@mail.ru>
2025-10-09 22:08:03 +0300

    fix: валидация ввода и бесконечный цикл

--- src/main/java/lab1/Main.java
+++ src/main/java/lab1/Main.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите запрос: ");
         String userRequest = scanner.nextLine();
 
         Request request = new Request(userRequest); // создали наш класс взаимодействия с запросом
@@ -17,6 +18,7 @@ public class Main {
 
         if (results.isEmpty()) {
             System.out.println("Ничего не найдено");
+            return;
         }
 
         System.out.println("Результаты: ");
@@ -24,15 +26,34 @@ public class Main {
             System.out.println(i + 1 + " " + results.get(i).getTitle());
         }
 
-        System.out.println("Выберите статью: ");
-        int choice = scanner.nextInt();
-        int pageId = results.get(choice - 1).getPageId();
-        String articleUrl = "https://ru.wikipedia.org/w/index.php?curid=" + pageId;
-        try {
-            Desktop.getDesktop().browse(new URI(articleUrl));
+
+        while (true) {
+            System.out.print("Выберите статью: (введите 0 если хотите завершить программу) ");
+            String input = scanner.nextLine().trim();
+            if (!input.matches("\\d+")) {
+                System.out.println("Введи число!");
+                continue;
+            }
+
+            int choice = Integer.valueOf(input);
+            if (choice == 0) break;
+            if (choice > results.size() || choice < 0) {
+                System.out.println("Неверный номер, введите заново");
+            }
+            else {
+                int pageId = results.get(choice - 1).getPageId();
+
+
+                String articleUrl = "https://ru.wikipedia.org/w/index.php?curid=" + pageId;
+                try {
+                    Desktop.getDesktop().browse(new URI(articleUrl));
+                }
+                catch (Exception e) {
+                    System.out.println("Ошибка при открытии браузера: " + e.getMessage());
+                }
+            }
+
         }
-        catch (Exception e) {
-            System.out.println("Ошибка при открытии браузера: " + e.getMessage());
-        }
+
     }
 }
